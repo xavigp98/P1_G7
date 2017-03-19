376f18ec163b6533a9a9156d529494c6bf80a09b
xavigp98 <xaviergraciapereniguez@enti.cat>
2017-03-19 20:44:16 +0100

    Creada la funcion que determina el nivel de dificultad y crea un mapa vacio con las dimensiones indicadas

--- CoinRace/Map.cpp
+++ CoinRace/Map.cpp
@@ -1,9 +1,31 @@
 #include "Map.h"
+#include <iostream>
 
 
 
 Map::Map()
 {
+	std::cout << "Escoge nivel de dificultad:" << std::endl << "Pulsa 1 para nivel facil" << std::endl << "Pulsa 2 para nivel normal" << std::endl << "Pulsa 3 para nivel dificil" << std::endl;
+
+	std::cin >> Map::difficulty;
+
+	Map::ROWS_MAP = 5 * Map::difficulty + rand() % ((5 * Map::difficulty * 2) - (5 * Map::difficulty));
+	Map::COLUMNS_MAP = 5 * Map::difficulty + rand() % ((5 * Map::difficulty * 2) - (5 * Map::difficulty));
+
+	Map::map = new cell*[Map::ROWS_MAP];
+
+	for (int i = 0; i < Map::ROWS_MAP; i++) {
+		Map::map[i] = new cell[Map::COLUMNS_MAP];
+	}
+
+	for (int i = 0; i < Map::ROWS_MAP; i++) {
+		for (int j = 0; j < Map::COLUMNS_MAP; j++) {
+			Map::map[i][j].hasCoin = 0;
+			Map::map[i][j].symbolToShow = '·';
+			Map::map[i][j].NUM_COLUMN = j;
+			Map::map[i][j].NUM_ROW = i;
+		}
+	}
 }
 
 
--- CoinRace/Map.h
+++ CoinRace/Map.h
@@ -2,7 +2,20 @@
 class Map
 {
 public:
+
+	int difficulty, ROWS_MAP,COLUMNS_MAP;
+
+	struct cell {
+		bool hasCoin;
+		char symbolToShow;
+		int NUM_COLUMN;
+		int NUM_ROW;
+	};
+
+	cell** map;
+
 	Map();
+
 	~Map();
 };
 
